79f6a9556d1b6d8e8531801671ad89f630eea4ab
Nick98765432 <131459397+Nick98765432@users.noreply.github.com>
2026-04-06 15:52:01 -0400

    added level 1-2

--- Levels/music.gd
+++ Levels/music.gd
@@ -3,7 +3,9 @@ extends AudioStreamPlayer
 
 
 func _ready() -> void:
-	intro.connect("music", _on_music)
-	
+	if not intro:
+		play()
+	else:
+		intro.connect("music", _on_music)
 func _on_music():
 	play()
