d0baad15673a409235be625fb2369020c5607284
Nick98765432 <131459397+Nick98765432@users.noreply.github.com>
2026-05-03 22:45:29 -0400

    level 2-1

--- Levels/LevelBuilderStuff/Scenes/useful stuff/hazards/explosive_barrel.gd
+++ Levels/LevelBuilderStuff/Scenes/useful stuff/hazards/explosive_barrel.gd
@@ -4,6 +4,9 @@ class_name explosiveBarrel
 var explosion: PackedScene = preload("res://Player/playerExplosion.tscn")
 var range: float = 40
 
+func _ready() -> void:
+	Signals.connect("destroy", _on_respawn)
+
 func explode():
 	for i in 5:
 		var rand = randf_range(-range, range)
@@ -14,4 +17,11 @@ func createExplosion(pos):
 	var boom = explosion.instantiate()
 	boom.global_position = pos
 	get_tree().get_root().add_child(boom)
-	queue_free()
+	hide()
+	$CollisionShape2D.disabled = true
+	set_collision_layer_value(1, false)
+
+func _on_respawn():
+	show()
+	set_collision_layer_value(1, true)
+	$CollisionShape2D.disabled = false
